d27ff3fee915b9f79d121470ecfe92f5df45ffba
EgorVasilev1 <egorvasilev652@gmail.com>
2024-10-06 18:40:41 +0300

    Неправильно отправлен репрозиторий

--- auth-service/src/main/java/ru/iot_cloud/security/Roles.java
+++
@@ -1,9 +0,0 @@
-package ru.iot_cloud.security;
-
-public final class Roles {
-    private Roles() { }
-    public static final String USER = "User";
-    public static final String SERVICE = "Service";
-    public static final String ADMIN = "Admin";
-    public static final String TESTER = "Tester";
-}
--- auth-service/src/main/java/ru/iot_cloud/user/UserDeviceHash.java
+++
@@ -1,9 +0,0 @@
-package ru.iot_cloud.user;
-
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-
-class UserDeviceHash extends PanacheEntity {
-    public Long userId;
-    public String deviceId;
-    public String hash;
-}
