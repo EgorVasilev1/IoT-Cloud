4fefb9c92a069d4387189fb6d275da4ccede07aa
EgorVasilev1 <egorvasilev652@gmail.com>
2024-10-11 20:57:02 +0300

    Delete microservices-java/auth-service directory

--- microservices-java/auth-service/src/main/java/ru/iot_cloud/user/DeviceVerificationResponse.java
+++
@@ -1,9 +0,0 @@
-package ru.iot_cloud.user;
-
-class DeviceVerificationResponse {
-    public String newDeviceHash;
-
-    public DeviceVerificationResponse(String newDeviceHash) {
-        this.newDeviceHash = newDeviceHash;
-    }
-}
